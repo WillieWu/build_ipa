2d2f37c2e586809ca98a935346fbb954cc9ab04e
Wow_我了个去 <601479318@qq.com>
2017-11-07 14:52:43 +0800

    fix xcode9 ipa

--- archiveipa.py
+++ archiveipa.py
@@ -21,21 +21,24 @@ project_name = 'YuDao.xcodeproj'
 IPA_dir = '/Users/wuhongbin/Desktop'
 
 yudao_target_name = 'YuDao'
-yudao_adhoc_fileName = 'XC iOS Ad Hoc: com.weiwei.yudao'
-yudao_adhoc_UUID = '6f527957-8597-442c-802c-70e30f02d589'
+yudao_adhoc_provisioning_profile_fileName = 'XC iOS Ad Hoc: com.weiwei.yudao'
+yudao_adhoc_provisioning_profile_UUID = '6f527957-8597-442c-802c-70e30f02d589'
 yudao_teamID = '26N4VZYSFU'
+yudao_bundleID = 'com.weiwei.yudao'
 
 hongyan_target_name = 'hongyan'
-hongyan_adhoc_fileName = 'hy_adhoc_new_device'
-hongyan_adhoc_UUID = '473b760a-3e59-426a-a0fb-14341f1fc526'
+hongyan_adhoc_provisioning_profile_fileName = 'hy_adhoc_new_device'
+hongyan_adhoc_provisioning_profile_UUID = '473b760a-3e59-426a-a0fb-14341f1fc526'
 hongyan_teamID = 'HDL3K2T9R8'
+hongyan_bundleID = 'com.weiwei.hy'
 
 miliao_target_name = 'miliao'
-miliao_adhoc_fileName = 'miliao_adhoc'
-miliao_adhoc_UUID = '3c6b14d6-84d3-48fc-a213-ff567d5c5897'
+miliao_adhoc_provisioning_profile_fileName = 'miliao_adhoc'
+miliao_adhoc_provisioning_profile_UUID = '3c6b14d6-84d3-48fc-a213-ff567d5c5897'
 miliao_teamID = '4DPS4FJPL9'
+miliao_bundleID = 'com.weiwei.miliao'
 
-def archive_IPA(set_target_name, set_provisioning_profile_UUID, teamID):
+def archive_IPA(set_target_name, set_provisioning_profile_UUID, teamID, set_bundleid, set_provisioning_profile_name):
 
 	print('\n', set_target_name, '\n', set_provisioning_profile_UUID, '\n', teamID)
 
@@ -51,6 +54,9 @@ def archive_IPA(set_target_name, set_provisioning_profile_UUID, teamID):
 		'teamID': teamID,
 		'method': 'ad-hoc',
 		'uploadSymbols': True,
+		'provisioningProfiles': {
+			set_bundleid: set_provisioning_profile_name
+		}
 	}
 	# 创建IPA plist文件
 	plistlib.writePlist(dic, plist_path)
@@ -81,12 +87,10 @@ def archive_IPA(set_target_name, set_provisioning_profile_UUID, teamID):
 
 	os.system('xcodebuild -exportArchive -archivePath %s -exportPath %s -exportOptionsPlist %s' % (archive_path, ipa_path, plist_path))
 
-def main():
+if __name__ == '__main__':
 
-	# archive_IPA('YuDao', yudao_adhoc_UUID)
-
-	# archive_IPA('hongyan', hongyan_adhoc_UUID)
+	print('Main')
+	archive_IPA(hongyan_target_name, hongyan_adhoc_provisioning_profile_UUID, hongyan_teamID, hongyan_bundleID, hongyan_adhoc_provisioning_profile_fileName)
 	
-	archive_IPA( miliao_target_name, miliao_adhoc_UUID, miliao_teamID)
 
-main()
+	
